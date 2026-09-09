d0e559374440dc80132a89b31fc4718f484e9115
Saumya  Brahmbhatt <139834472+Saumya812@users.noreply.github.com>
2026-09-09 18:58:52 -0400

    Add failpass_fixes folder with new fixed files for cases 772 790 827 858 901 921

---
+++ Part2_Create_test/reconstructed_cases/failpass_fixes/case_901_new_fix.py
@@ -0,0 +1,18 @@
+﻿from qiskit.circuit import QuantumCircuit
+from qiskit import transpile
+
+qc = QuantumCircuit(2)
+qc.h(0)
+qc.s(0)
+qc.cx(0, 1)
+
+# S = T^2 so replace S with two T gates
+qc2 = QuantumCircuit(2)
+qc2.h(0)
+qc2.t(0)
+qc2.t(0)
+qc2.cx(0, 1)
+
+basis_gates = ["h", "t", "cx", "id"]
+qc_transpiled = transpile(qc2, basis_gates=basis_gates)
+print(qc_transpiled)
---
+++ Part2_Create_test/reconstructed_cases/failpass_fixes/case_921_new_fix.py
@@ -0,0 +1,7 @@
+﻿from qiskit.quantum_info import SparsePauliOp
+import numpy as np
+
+op = SparsePauliOp.from_list([("ZZII", 1.0)])
+matrix = op.to_matrix()
+eigenvalues = np.linalg.eigvalsh(matrix)
+print("Minimum eigenvalue:", eigenvalues[0])
