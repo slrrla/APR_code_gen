4972b8e4211aa880d336b54da8646074801f56bb
Mitali Hukkeri <mhukker1@umbc.edu>
2026-09-10 10:21:15 -0400

    Update buggy.py

--- Part2_Create_test/reconstructed_cases/issue_924/buggy.py
+++ Part2_Create_test/reconstructed_cases/issue_924/buggy.py
@@ -1,10 +1,32 @@
 import numpy as np
-from qiskit.quantum_info import Statevector, SparsePauliOp
+from qiskit.quantum_info import Statevector
 
-O = SparsePauliOp(data=["II", "IZ", "ZI", "ZZ", "XX"], coeffs=[-1, 0.4, -0.4, -1, 0.1])
+try:
+    from qiskit.opflow import PauliSumOp
+    O = PauliSumOp.from_list([
+        ("II", -1),
+        ("IZ", 0.4),
+        ("ZI", -0.4),
+        ("ZZ", -1),
+        ("XX", 0.1),
+    ])
+except ImportError:
+    from qiskit.quantum_info import SparsePauliOp
+    O = SparsePauliOp.from_list([
+        ("II", -1),
+        ("IZ", 0.4),
+        ("ZI", -0.4),
+        ("ZZ", -1),
+        ("XX", 0.1),
+    ])
 
-psi = np.sqrt(1/2) * (Statevector.from_label('00') + Statevector.from_label('11'))
+psi = (
+    Statevector.from_label("00") +
+    Statevector.from_label("11")
+) / np.sqrt(2)
 
-# trying to compute <psi|O^2|psi> but forgot to square the operator
-exp_val = psi.expectation_value(O)
-print(exp_val)
+# Bug: computes <psi|O|psi>, not <psi|O^2|psi>
+operator_matrix = O.to_matrix()
+expectation_value = psi.expectation_value(operator_matrix)
+
+print(expectation_value)
